1e00ef3ccbd9e82b275f8ab74a3805161797463f
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-01-09 13:53:53 +0000

    ..
    ..

--- app/src/main/java/com/example/eventos/MainActivity.kt
+++ app/src/main/java/com/example/eventos/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.eventos
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Adicionar o PrimeiroFragment como o Fragment inicial
-        if (savedInstanceState == null) {
-            val fragment = PrimeiroFragment()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit()
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
         }
-    }
+
+        fun navegarParaSegunda(view: View) {
+            val intent = Intent(this, SegundaActivity::class.java)
+            startActivity(intent)
+        }
 }
