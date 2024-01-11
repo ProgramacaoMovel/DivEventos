bb1e238cca00bb9c7d1c3076eebac9d52fb0093b
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2024-01-11 23:08:01 +0000

    Pagina de Registo

--- app/src/main/java/com/example/eventos/signinActivity.kt
+++ app/src/main/java/com/example/eventos/signinActivity.kt
@@ -1,26 +1,22 @@
 package com.example.eventos
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
-
+class signinActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signin)
 
-        val btnNavegarParaSegunda: Button = findViewById(R.id.btnNavegarParaSegunda)
-        val btnNavReg : Button = findViewById((R.id.btnreg))
-        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+        val btnNavegarParaSegunda: Button = findViewById(R.id.signin_log)
+        auth = FirebaseAuth.getInstance()
 
         btnNavegarParaSegunda.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        btnNavReg.setOnClickListener{
-            val intent = Intent(this, signinActivity :: class.java)
             startActivity(intent)
         }
     }
