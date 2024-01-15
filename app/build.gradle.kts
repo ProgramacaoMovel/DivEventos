9d1addeb96e60510dc11fde0ffde232fe185f742
ludvikcruz <lucas.2001.cruz@gmail.com>
2024-01-15 21:40:12 +0000

    afasf
    afasfasfasf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,25 +36,30 @@ android {
     }
 }
 
-dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
-    implementation("com.google.firebase:firebase-analytics:21.5.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("androidx.activity:activity-ktx:1.2.0")
-    implementation("androidx.fragment:fragment-ktx:1.3.0")
 
-}
+    dependencies {
+
+        implementation("androidx.core:core-ktx:1.9.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.11.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
+        implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
+        implementation("com.google.firebase:firebase-database:20.3.0")
+        implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+        implementation("com.google.firebase:firebase-analytics:21.5.0")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+        implementation("com.google.firebase:firebase-analytics-ktx")
+        implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+        implementation("com.google.firebase:firebase-auth-ktx")
+        implementation("androidx.activity:activity-ktx:1.2.0")
+        implementation("androidx.fragment:fragment-ktx:1.3.0")
+        implementation("com.google.firebase:firebase-database-ktx")
+
+
+    }
+
