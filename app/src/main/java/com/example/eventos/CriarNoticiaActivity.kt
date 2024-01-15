28a5e09286e4c0a0a4b52c731858b9cc7ab22a7e
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-01-15 19:10:10 +0000

    koytlin
    kbohu

--- app/src/main/java/com/example/eventos/CriarNoticiaActivity.kt
+++ app/src/main/java/com/example/eventos/CriarNoticiaActivity.kt
@@ -1,10 +1,13 @@
 package com.example.eventos
 
+
+
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
+import android.view.View  // Importação adicionada
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -17,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.firebase.database.FirebaseDatabase
+
 
 class CriarNoticiaActivity : AppCompatActivity() {
 
@@ -35,17 +39,25 @@ class CriarNoticiaActivity : AppCompatActivity() {
         val localizacaoEditText: EditText = findViewById(R.id.create_loc_noticia)
         val textoEditText: EditText = findViewById(R.id.create_corpo_noticia)
         val criarNoticiaButton: Button = findViewById(R.id.criarNoticiaButton)
+        val buttonVoltar: Button = findViewById(R.id.buttonVoltar)
 
         imagePickerLauncher =
             registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
                 imageUri = uri
-                // Aqui você pode, por exemplo, exibir a imagem selecionada em um ImageView
             }
 
         val selectImageButton = findViewById<Button>(R.id.btnSelectImage)
         selectImageButton.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                )
             } else {
                 imagePickerLauncher.launch("image/*")
             }
@@ -68,11 +80,14 @@ class CriarNoticiaActivity : AppCompatActivity() {
                 id: Long
             ) {
                 val selectedItem = parent.getItemAtPosition(position).toString()
-                // Faça algo com o item selecionado
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
-                Toast.makeText(this@CriarNoticiaActivity, "Tem que selecionar uma categoria para prosseguir.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@CriarNoticiaActivity,
+                    "Tem que selecionar uma categoria para prosseguir.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
@@ -80,57 +95,67 @@ class CriarNoticiaActivity : AppCompatActivity() {
             val titulo = tituloEditText.text.toString().trim()
             val localizacao = localizacaoEditText.text.toString().trim()
             val texto = textoEditText.text.toString().trim()
-
-
-            if (titulo.isNotEmpty() && localizacao.isNotEmpty() && texto.isNotEmpty()) {
-                val noticia = dbModel.Noticias(
+            if (titulo.isNotEmpty() && localizacao.isNotEmpty() && texto.isNotEmpty() && imageUri != null) {
+                val noticia = Noticia(
                     titulo,
                     localizacao,
                     texto,
                     imageUri.toString()
-                ) // Substitua Noticia pela sua classe de modelo
+                )
                 enviarNoticiaParaFirebase(noticia)
             } else {
-                if (imageUri == null) {
-                    Toast.makeText(this, "Tem que selecionar uma imagem para prosseguir.", Toast.LENGTH_SHORT).show()
-                } else {
-                Toast.makeText(this, "Por favor, preencha todos os campos.", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(
+                    this,
+                    "Por favor, preencha todos os campos e selecione uma imagem.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+        }
 
+        buttonVoltar.setOnClickListener {
+            finish()
         }
     }
-    }
 
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     imagePickerLauncher.launch("image/*")
                 } else {
-                    Toast.makeText(this, "Permissão de acesso ao armazenamento é necessária para selecionar imagens.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "Permissão de acesso ao armazenamento é necessária para selecionar imagens.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
     }
 
-    private fun enviarNoticiaParaFirebase(noticia: dbModel.Noticias) { // Substitua Noticia pela sua classe de modelo
+    private fun enviarNoticiaParaFirebase(noticia: Noticia) {
         val databaseReference = FirebaseDatabase.getInstance().getReference("noticias")
-        val noticiaId = databaseReference.push().key // Cria um ID único
-        noticia.notId =
-            noticiaId ?: "" // Certifique-se de que sua classe Noticia tenha um campo 'id'
+
+        val noticiaId = databaseReference.push().key
+        noticia.id = noticiaId ?: ""
+
 
         databaseReference.child(noticiaId ?: "").setValue(noticia)
             .addOnSuccessListener {
                 Toast.makeText(this, "Notícia criada com sucesso!", Toast.LENGTH_SHORT).show()
-                finish() // Fecha a atividade após o sucesso
+                finish()
             }
             .addOnFailureListener {
                 Toast.makeText(this, "Erro ao criar notícia.", Toast.LENGTH_SHORT).show()
             }
+
     }
+
     data class Noticia(
         val titulo: String,
         val localizacao: String,
