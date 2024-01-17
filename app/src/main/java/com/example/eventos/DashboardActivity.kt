c25ba3ac4c6004cde9bca9731c2e32478654b0fc
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2024-01-17 19:09:13 +0000

    ty love

--- app/src/main/java/com/example/eventos/DashboardActivity.kt
+++ app/src/main/java/com/example/eventos/DashboardActivity.kt
@@ -1,20 +1,16 @@
 package com.example.eventos
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.navigation.NavigationView
 import android.widget.Button
-import com.example.eventos.R.id.nav_view
-import com.example.eventos.R.layout.menufragment
+import com.example.eventos.R.id.conadunto
+
 
 class DashboardActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +22,40 @@ class DashboardActivity : AppCompatActivity() {
             val intent = Intent(this, CriarNoticiaActivity::class.java)
             startActivity(intent)
         }
+        val toolbar: Toolbar = findViewById(conadunto)
+        setSupportActionBar(toolbar)
+
+        // Em suas atividades
+        fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_dash, menu)
+            return true
+        }
+        fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.dashboard -> {
+                    val intent = Intent(this, DashboardActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.highlights -> {
+                    val intent = Intent(this, HeadlinesActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.seguir -> {
+                    val intent = Intent(this, FeedbackActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.feedback -> {
+                    val intent = Intent(this, FeedbackActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+
 
     }
 }
